10d08ed63d870f6930dd681be081acf589cda93f
christianzhou1 <christianzhiruizhou@outlook.com>
2024-08-09 11:33:38 -0400

    cache quote every minute to reduce number of calls

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,16 +6,26 @@
   </div>
 </template>
 <script>
+// import _ from 'lodash'
+
 export default {
   data() {
     return {
       quote: null,
       apiKey: import.meta.env.VITE_X_API_KEY,
-      category: 'happiness'
+      category: 'happiness',
+      refreshInterval: 60 * 1000
     }
   },
   methods: {
     async fetchQuote(category) {
+      const lastFetch = localStorage.getItem('lastFetch')
+      const cachedData = localStorage.getItem('quote')
+      if (lastFetch && Date.now() - lastFetch < this.refreshInterval && cachedData) {
+        this.quote = JSON.parse(cachedData)
+        console.log('using cached data', this.quote)
+        return
+      }
       try {
         const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
           headers: {
@@ -25,7 +35,10 @@ export default {
         if (!response.ok) {
           throw new Error('Network response was not ok')
         }
-        this.quote = await response.json()
+        const data = await response.json()
+        this.quote = data
+        localStorage.setItem('lastFetch', Date.now())
+        localStorage.setItem('quote', JSON.stringify(data))
       } catch (error) {
         console.error(error)
       }
